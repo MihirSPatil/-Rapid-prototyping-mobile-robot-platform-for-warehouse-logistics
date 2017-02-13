3820873f9a856ee1b7f605c82a92e3bfe83866bf
Mihir <mihir.s456@gmail.com>
2017-02-13 14:52:07 +0530

    added combined launch file with a latcher

--- agv_teleop/src/agv_teleop_node.cpp
+++ agv_teleop/src/agv_teleop_node.cpp
@@ -10,116 +10,116 @@
 #define KEYCODE_D 0x42
 #define KEYCODE_Q 0x71
 
-class TeleopTurtle{
+class TeleopTurtle {
 public:
-  TeleopTurtle();
-  void keyLoop();
+TeleopTurtle();
+void keyLoop();
 
 private:
 
 
-  ros::NodeHandle nh_;
-  double linear_, angular_, l_scale_, a_scale_;
-  ros::Publisher twist_pub_;
+ros::NodeHandle nh_;
+double linear_, angular_, l_scale_, a_scale_;
+ros::Publisher twist_pub_;
 
 };
 
-TeleopTurtle::TeleopTurtle():
-  linear_(0),
-  angular_(0),
-  l_scale_(2.0),
-  a_scale_(2.0){
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
+TeleopTurtle::TeleopTurtle() :
+        linear_(0),
+        angular_(0),
+        l_scale_(2.0),
+        a_scale_(2.0){
+        nh_.param("scale_angular", a_scale_, a_scale_);
+        nh_.param("scale_linear", l_scale_, l_scale_);
 
-  twist_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+        twist_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_1", 1);
 }
 
 int kfd = 0;
 struct termios cooked, raw;
 
 void quit(int sig){
-  (void)sig;
-  tcsetattr(kfd, TCSANOW, &cooked);
-  ros::shutdown();
-  exit(0);
+        (void)sig;
+        tcsetattr(kfd, TCSANOW, &cooked);
+        ros::shutdown();
+        exit(0);
 }
 
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "agv_teleop_node");
-  TeleopTurtle teleop_turtle;
+        ros::init(argc, argv, "agv_teleop_node");
+        TeleopTurtle teleop_turtle;
 
-  signal(SIGINT,quit);
+        signal(SIGINT,quit);
 
-  teleop_turtle.keyLoop();
+        teleop_turtle.keyLoop();
 
-  return(0);
+        return(0);
 }
 
 
 void TeleopTurtle::keyLoop(){
-  char c;
-  bool dirty=false;
+        char c;
+        bool dirty=false;
 
 
-  // get the console in raw mode
-  tcgetattr(kfd, &cooked);
-  memcpy(&raw, &cooked, sizeof(struct termios));
-  raw.c_lflag &=~ (ICANON | ECHO);
-  // Setting a new line, then end of file
-  raw.c_cc[VEOL] = 1;
-  raw.c_cc[VEOF] = 2;
-  tcsetattr(kfd, TCSANOW, &raw);
+        // get the console in raw mode
+        tcgetattr(kfd, &cooked);
+        memcpy(&raw, &cooked, sizeof(struct termios));
+        raw.c_lflag &=~(ICANON | ECHO);
+        // Setting a new line, then end of file
+        raw.c_cc[VEOL] = 1;
+        raw.c_cc[VEOF] = 2;
+        tcsetattr(kfd, TCSANOW, &raw);
 
-  puts("Reading from keyboard");
-  puts("---------------------------");
-  puts("Use arrow keys to move the turtle.");
-
-
-  for(;;){
-    // get the next event from the keyboard
-    if(read(kfd, &c, 1) < 0){
-      perror("read():");
-      exit(-1);
-    }
-
-    linear_=angular_=0;
-    ROS_DEBUG("value: 0x%02X\n", c);
-
-    switch(c){
-      case KEYCODE_L:
-        ROS_DEBUG("LEFT");
-        angular_ = 1.0;
-        dirty = true;
-        break;
-      case KEYCODE_R:
-        ROS_DEBUG("RIGHT");
-        angular_ = -1.0;
-        dirty = true;
-        break;
-      case KEYCODE_U:
-        ROS_DEBUG("UP");
-        linear_ = 1.0;
-        dirty = true;
-        break;
-      case KEYCODE_D:
-        ROS_DEBUG("DOWN");
-        linear_ = -1.0;
-        dirty = true;
-        break;
-    }
+        puts("Reading from keyboard");
+        puts("---------------------------");
+        puts("Use arrow keys to move the Bantam.");
 
 
-    geometry_msgs::Twist twist;
-    twist.angular.z = a_scale_*angular_;
-    twist.linear.x = l_scale_*linear_;
-    if(dirty ==true){
-      twist_pub_.publish(twist);
-      dirty=false;
-    }
-  }
+        for(;; ) {
+                // get the next event from the keyboard
+                if(read(kfd, &c, 1) < 0) {
+                        perror("read():");
+                        exit(-1);
+                }
+
+                linear_=angular_=0;
+                ROS_DEBUG("value: 0x%02X\n", c);
+
+                switch(c) {
+                case KEYCODE_L:
+                        ROS_DEBUG("LEFT");
+                        angular_ = 1.0;
+                        dirty = true;
+                        break;
+                case KEYCODE_R:
+                        ROS_DEBUG("RIGHT");
+                        angular_ = -1.0;
+                        dirty = true;
+                        break;
+                case KEYCODE_U:
+                        ROS_DEBUG("UP");
+                        linear_ = 1.0;
+                        dirty = true;
+                        break;
+                case KEYCODE_D:
+                        ROS_DEBUG("DOWN");
+                        linear_ = -1.0;
+                        dirty = true;
+                        break;
+                }
 
 
-  return;
+                geometry_msgs::Twist twist;
+                twist.angular.z = a_scale_*angular_;
+                twist.linear.x = l_scale_*linear_;
+                if(dirty ==true) {
+                        twist_pub_.publish(twist);
+                        dirty=false;
+                }
+        }
+
+
+        return;
 }
