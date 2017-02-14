b156e33b0af02140a36bc9f368105806437f8335
Mihir <mihir.s456@gmail.com>
2017-02-14 08:39:54 +0530

    added stop functionality in gazebo sim

--- agv_teleop/src/teleop_latcher.cpp
+++ agv_teleop/src/teleop_latcher.cpp
@@ -6,24 +6,23 @@ ros::Publisher twist_pub;
 geometry_msgs::Twist vel;
 
 void cb(const geometry_msgs::Twist msg) {
-        vel = msg;
+  vel = msg;
 }
 
-
 int main(int argc, char **argv) {
-        ros::init(argc, argv, "teleop_latcher");
-        ros::NodeHandle n;
+  ros::init(argc, argv, "teleop_latcher");
+  ros::NodeHandle n;
 
-        ros::Subscriber sub = n.subscribe<geometry_msgs::Twist>("/cmd_vel_1", 50,cb);
-        twist_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 50);
+  ros::Subscriber sub = n.subscribe<geometry_msgs::Twist>("/cmd_vel_1", 50, cb);
+  twist_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 50);
 
-        ros::Rate rate(40);
+  ros::Rate rate(50);
 
-        while (n.ok()) {
-                ros::spinOnce();
+  while (n.ok()) {
+    ros::spinOnce();
 
-                twist_pub.publish(vel);
+    twist_pub.publish(vel);
 
-                rate.sleep();
-        }
+    rate.sleep();
+  }
 }
